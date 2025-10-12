c15c330886951367cca51c35afd440eb9e0de91e
matias pizarro <matias@pizarro.net>
2025-10-12 23:33:14 +0200

    Automate pkg updates and replication of 14.3 templates, though *.snap versions will need repo fine-tuning
    (cherry picked from commit bec1589838f26bfe8579cf0c4afe79df7af5b2bc)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,111 @@
 #!/usr/bin/env sh
 
 base_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && cd .. && pwd)
-
-freebsd_major="${3:-14}"
-freebsd_minor="${4:-3}"
-template="${1:-base}"
-template_version="${2}"
 scripts_dir="${base_dir}/scripts"
+
+usage() {
+    cat << 'EOF'
+
+build.sh
+========
+
+Usage: build.sh [-M <freebsd-major-version>] [-m <freebsd-minor-version>] [-t <container-template>] [-v <container-template-version>] [-h]
+
+Options:
+    -M         FreeBSD Major version (default: 14)
+    -m         FreeBSD Minor version (default: snap)
+    -t         Container template (default: base)
+    -v         Container template version (default: <none>)
+
+Examples:
+    $ scripts/build.sh
+
+    # Building freebsd-base for FreeBSD 14.3
+    #     context: freebsd_containers/freebsd/14.3/base
+    #     file   : freebsd_containers/freebsd/14.3/base/Containerfile
+
+    $ scripts/build.sh -M 14 -m 2 -t zfs
+    # Building freebsd-zfs for FreeBSD 14.2
+    #     context: freebsd_containers/freebsd/14.2/zfs
+    #     file   : freebsd_containers/freebsd/14.2/zfs/Containerfile
+
+    $ scripts/build.sh -M 16 -t node -v 20
+
+    # Building freebsd-node20 for FreeBSD 16.snap
+    #     context: freebsd_containers/freebsd/16.snap/node/20
+    #     file   : freebsd_containers/freebsd/16.snap/node/20/Containerfile
+
+EOF
+    exit 1
+}
+
+while getopts "M:m:t:v:h" FLAG; do
+    case "${FLAG}" in
+    M)
+        freebsd_major="${OPTARG}"
+        ;;
+    m)
+        freebsd_minor="${OPTARG}"
+        ;;
+    t)
+        template="${OPTARG}"
+        ;;
+    v)
+        template_version="${OPTARG}"
+        ;;
+    h)
+        usage
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+freebsd_major="${freebsd_major:-14}"
+if [ "${freebsd_major}" == "14" ]; then
+    freebsd_minor="${freebsd_minor:-3}"
+else
+    freebsd_minor="${freebsd_minor:-snap}"
+fi
+if [ "${freebsd_minor}" == "snap" ]; then
+    freebsd_minor_digit="0"
+else
+    freebsd_minor_digit="${freebsd_minor}"
+fi
+template="${template:-base}"
+template_version="${template_version}"
+
 template_dir="${base_dir}/freebsd/${freebsd_major}.${freebsd_minor}/${template}"
 if [ -n "${template_version}" ]; then
-  template_dir="${template_dir}/${template_version}"
+    template_dir="${template_dir}/${template_version}"
 fi
 containerfile="${template_dir}/Containerfile"
 
 # echo ${scripts_dir}
 # echo ${template_dir}
+
+echo
+echo "# Building freebsd-${template}${template_version} for FreeBSD ${freebsd_major}.${freebsd_minor}"
+echo "#     context: ${template_dir}"
+echo "#     file   : ${containerfile}"
+echo
+
+if [ ! -f "${containerfile}" ]; then
+    echo "${containerfile}" does not exist
+    exit 1
+fi
+
 cd ${template_dir}
 
 podman build \
-  --volume /usr/local/sbin/pkg:/bin/pkg \
-  --env IGNORE_OSVERSION=yes \
-  --env ABI=FreeBSD:${freebsd_major}:$(sysctl -n hw.machine_arch) \
-  --env OSVERSION=${freebsd_major}0${freebsd_minor}000 \
-  --no-hosts \
-  --tag freebsd-${template}${template_version}:${freebsd_major}.${freebsd_minor} \
-  --file Containerfile
+    --volume /usr/local/sbin/pkg:/bin/pkg \
+    --env IGNORE_OSVERSION=yes \
+    --env ABI=FreeBSD:${freebsd_major}:$(sysctl -n hw.machine_arch) \
+    --env OSVERSION=${freebsd_major}0${freebsd_minor_digit}000 \
+    --env VERSION_MINOR=${freebsd_minor_digit} \
+    --no-hosts \
+    --tag freebsd-${template}${template_version}:${freebsd_major}.${freebsd_minor} \
+    --file Containerfile
---
+++ scripts/replicate_templates.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+
+base_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && cd .. && pwd)
+scripts_dir="${base_dir}/scripts"
+
+base_version="14.3"
+base_version_dir="${base_dir}/freebsd/${base_version}"
+base_image_uri="$(cat ${base_version_dir}/base_image)"
+echo ${base_image_uri}
+
+cd ${base_version_dir}
+for elements in $(find . -name "Containerfile"); do
+    template="$(echo ${elements} | awk -F '/' '{print $2}')"
+    next="$(echo ${elements} | awk -F '/' '{print $3}')"
+    if [ "${next}" == "Containerfile" ]; then
+        version=""
+        template_parent_dir=""
+        template_dir="${template}"
+    else
+        version="${next}"
+        template_parent_dir="/${template}"
+        template_dir="${template}/${version}"
+    fi
+    base_template_dir="${base_version_dir}/${template_dir}"
+    for target_version in 14.snap 15.snap 16.snap; do
+        target_version_dir="${base_dir}/freebsd/${target_version}"
+        target_template_parent_dir="${target_version_dir}${template_parent_dir}"
+        target_template_dir="${target_version_dir}/${template_dir}"
+        mkdir -p "${target_version_dir}${template_parent_dir}"
+        rsync -avz "${base_template_dir}" "${target_template_parent_dir}"
+        target_base_image_uri="$(cat ${target_version_dir}/base_image)"
+        sed 's|'"${base_image_uri}"'|'"${target_base_image_uri}"'|g' "${base_template_dir}/Containerfile" > "${target_template_dir}/Containerfile"
+    done
+done
+
+exit
+for template in $(\ls "${base_version_dir}"); do
+    template_dir="${base_version_dir}/${template}"
+    if [ -d "${template_dir}" ]; then
+        echo "${template_dir}"
+        for target_version in 14.snap 15.snap 16.snap; do
+            target_version_dir="${base_dir}/freebsd/${target_version}"
+        done
+    fi
+done
