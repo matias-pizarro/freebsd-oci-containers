3a484aa784f4e3c2f1e223ba3f70a7b63c299e32
matias pizarro <matias@pizarro.net>
2025-10-14 16:55:11 +0200

    feat(scripts/build_all_images.sh): add a script that builds (naively) all images
    (cherry picked from commit 5a4fbd9e44378d023b63b8aac2008512a6dcef04)

---
+++ scripts/build_all_images.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+base_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && cd .. && pwd)
+scripts_dir="${base_dir}/scripts"
+
+${scripts_dir}/build.sh ${scripts_dir}/build.sh \
+    && ${scripts_dir}/build.sh -t zfs \
+    && ${scripts_dir}/build.sh -t node -v 20 \
+    && ${scripts_dir}/build.sh -t node -v 22 \
+    && ${scripts_dir}/build.sh -t node -v 24 \
+    && ${scripts_dir}/build.sh -t postgres -v 13 \
+    && ${scripts_dir}/build.sh -t postgres -v 14 \
+    && ${scripts_dir}/build.sh -t postgres -v 15 \
+    && ${scripts_dir}/build.sh -t postgres -v 16 \
+    && ${scripts_dir}/build.sh -t postgres -v 17 \
+    && ${scripts_dir}/build.sh -t postgres -v 18
