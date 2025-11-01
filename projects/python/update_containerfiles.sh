adf86e3a8102401395adcecc38c60853e06567b8
matias pizarro <matias@pizarro.net>
2025-11-01 23:06:19 +0100

    Add version.sh script to create all versions
    (cherry picked from commit 69638de1e20203183f8607b649887fdec277ffbe)

---
+++ projects/python/update_containerfiles.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+base_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && cd .. && cd .. && pwd)
+
+# ${base_dir}/upstreams/python/update.sh
--- scripts/replicate_templates.sh
+++ scripts/replicate_templates.sh
@@ -5,8 +5,6 @@ scripts_dir="${base_dir}/scripts"
 
 base_version="14.3"
 base_version_dir="${base_dir}/freebsd/${base_version}"
-base_image_uri="$(cat ${base_version_dir}/base_image)"
-echo ${base_image_uri}
 
 cd ${base_version_dir}
 for elements in $(find . -name "Containerfile"); do
@@ -28,7 +26,5 @@ for elements in $(find . -name "Containerfile"); do
         target_template_dir="${target_version_dir}/${template_dir}"
         mkdir -p "${target_version_dir}${template_parent_dir}"
         rsync -avz "${base_template_dir}" "${target_template_parent_dir}"
-        target_base_image_uri="$(cat ${target_version_dir}/base_image)"
-        sed 's|'"${base_image_uri}"'|'"${target_base_image_uri}"'|g' "${base_template_dir}/Containerfile" > "${target_template_dir}/Containerfile"
     done
 done
